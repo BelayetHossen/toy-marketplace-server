c283264124151c6304a8d7350b5c906dd1b797ab
BelayetHossen <ntarget50@gmail.com>
2023-05-21 02:02:17 +0600

    details data load for single toy

--- index.js
+++ index.js
@@ -87,6 +87,13 @@ async function run() {
     res.send(result);
   })  
 
+  app.get('/toy/details/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await toyCollection.findOne(query);
+    res.send(result);
+  })  
+
 app.patch('/toy/update', async (req, res) => {
   
   const updateToy = req.body;
